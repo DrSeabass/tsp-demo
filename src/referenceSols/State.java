167deeb0063da2efaa36683ceb8c39137030abd3
Jordan Thayer <jthayer@draper.com>
2015-10-02 13:46:59 -0400

    Forgot to add the state for DFS

---
+++ src/referenceSols/State.java
@@ -0,0 +1,10 @@
+package referenceSols;
+
+public class State{
+    public int city;
+    public float legCost;
+    public State(int city, float legCost){
+        this.city = city;
+        this.legCost = legCost;
+    }
+}
